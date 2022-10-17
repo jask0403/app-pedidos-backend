4453af5d7da43db0dd574bfd5ba9e2ab77a8c650
jask0403 <jask0403@hotmail.com>
2022-10-17 13:45:59 -0500

    Version 1.0.0 de la aplicación de pedidos

---
+++ Ciclo4/pedidos/src/models/producto.model.ts
@@ -0,0 +1,43 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Producto extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  precio: number;
+
+  @property({
+    type: 'string',
+  })
+  imagen?: string;
+
+  @property({
+    type: 'string',
+  })
+  pedidoId?: string;
+
+  constructor(data?: Partial<Producto>) {
+    super(data);
+  }
+}
+
+export interface ProductoRelations {
+  // describe navigational properties here
+}
+
+export type ProductoWithRelations = Producto & ProductoRelations;
